6847cc351400e13ed3e5c178b5bfb70f8fcd21c9
Sullivan0104 <tsullivan0104@gmail.com>
2025-11-27 12:47:37 -0500

    added system clear call when steping through a input.

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 using namespace std;
 
+void clearScreen(){
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+}
+
 Controller::Controller(CPUModel& m,bool step):model(m),stepMode(step){}
 
 void Controller::run()
@@ -14,6 +22,8 @@ void Controller::run()
             cout<<"Press Enter for next instruction (q to quit)\n";
             string s; getline(cin,s);
             if(!s.empty()&&(s[0]=='q'||s[0]=='Q')) break;
+            clearScreen();
+
         }
     }
     cout<<"Simulation ended. Cycles="<<model.cycles<<"\n";
